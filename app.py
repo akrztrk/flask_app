c815073e3d7c98b2083538f86a0da81941af4c27
akar <akarozturk1@gmail.com>
2021-01-17 18:49:17 +0300

    first-commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+# http://127.0.0.1:5000/users/2 -> Base Url
+from flask import Flask
+app =  Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello Flask'
+app.run()
+@app.route('/users/<user_id>' , methods = ['GET','POST','PUT','DELETE'])
+def parameter(user_id):
+    if request.method == 'GET':
+        return 'GET method'
+    elif request.method == 'POST':
+        data = request.form
+        return data
+    elif request.method == 'PUT':
+        return 'Put method'
+    elif request.method == 'DELETE':
+        return 'Delete method'
+    else:
+        return 'Not allowed method'
+    return user_id
+app.run()
